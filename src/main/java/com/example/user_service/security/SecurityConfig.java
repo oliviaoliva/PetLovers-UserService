fbdc48a3dcb4f10475a52898536278b3481dd57d
oliviaoliva <olivia.costa.oliva@icloud.com>
2025-01-31 10:28:24 -0300

    :construction: feat: adding cors settings

--- src/main/java/com/example/user_service/security/SecurityConfig.java
+++ src/main/java/com/example/user_service/security/SecurityConfig.java
@@ -2,16 +2,20 @@ package com.example.user_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.authentication.AuthenticationManager;
-// import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-// import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 // import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 // import org.springframework.beans.factory.annotation.Autowired;
-
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
    
 @Configuration
 public class SecurityConfig {
@@ -34,10 +38,23 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll() // 🔓 Permite todas as requisições sem autenticação
-            );
+                .anyRequest().permitAll() 
+            )
+            .cors(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
     }
 
     // @Bean
